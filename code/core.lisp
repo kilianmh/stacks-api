562f06d194c6b78c44bf0aedc47813fb21aab913
kilianmh <kilian.haemmerle@protonmail.com>
2022-07-28 20:28:17 +0200

    Replace lexical functions with global functions
    - get-host (from get-stacks-uri)
    - get-array-symbol (from get-query)
    - get-symbols-from-list (from get-function)
    
    -> Shorter functions & macro
    -> Increase readability & modularity

--- code/core.lisp
+++ code/core.lisp
@@ -1,14 +1,15 @@
 (in-package #:stacks-api)
 
+(defun* (get-host -> (simple-array character (33))) ()
+  "Generate host string for mainnet and testnet"
+  (cond ((string= *network* "mainnet") "stacks-node-api.mainnet.stacks.co")
+        ((string= *network* "testnet") "stacks-node-api.testnet.stacks.co")
+        (t (error "not a valid network identifier"))))
+
 (defun* (get-stacks-uri -> (values uri-https &optional))
     ((path (or string list)) &optional ((query list)()))
   "Generate REST uri"
-  (flet* (((get-host -> (simple-array character (33)))
-           () "Generate host string for mainnet and testnet"
-           (cond ((string= *network* "mainnet") "stacks-node-api.mainnet.stacks.co")
-                 ((string= *network* "testnet") "stacks-node-api.testnet.stacks.co")
-                 (t (error "not a valid network identifier")))))
-         (make-uri :scheme "https" :host (get-host) :path path :query query)))
+  (make-uri :scheme "https" :host (get-host) :path path :query query))
 
 (defun* (list-to-alist -> association-list) (&rest query)
   "Generate alist from list"
@@ -37,26 +38,27 @@
     `(,@(mapcan (lambda (x) `((,x ,(get-slot x 'type))))
                 mandatory-symbols)
       &key ,@(mapcar (lambda (x) `((,x (or null ,(get-slot x 'type))) ()))
-                     optional-symbols))))
+                     optional-symbols)))
+
+  (defun* (get-array-symbol -> symbol) ((query-symbols proper-list))
+    (car (remove nil
+                 (mapcar (lambda (x)
+                           (case (get-slot x 'type)
+                                 ((transaction-list type-list asset-identifier-list) x)
+                                 (otherwise nil)))
+                         query-symbols)))))
 
 (defmacro get-query (query-symbols)
   "Generate code for handling of query parameters. 
    Result is different for an array/list parameters."
-  (flet* (((get-array-symbol -> symbol) ((query-symbols proper-list))
-           (car (remove nil
-                        (mapcar (lambda (x)
-                                  (case (get-slot x 'type)
-                                    ((transaction-list type-list asset-identifier-list) x)
-                                    (otherwise nil)))
-                                query-symbols)))))
-         (*let ((array-symbol symbol (get-array-symbol query-symbols))
-                (query-symbols proper-list (remove array-symbol query-symbols))
-                (list-alist proper-list (get-list-to-alist query-symbols)))
-               (if array-symbol
-                   `(append (iter (for x in ,array-symbol)
-                                  (collect (cons ,(downcase (symbol-name array-symbol)) x)))
-                            ,list-alist)
-                   list-alist))))
+  (*let ((array-symbol symbol (get-array-symbol query-symbols))
+         (query-symbols proper-list (remove array-symbol query-symbols))
+         (list-alist proper-list (get-list-to-alist query-symbols)))
+        (if array-symbol
+            `(append (iter (for x in ,array-symbol)
+                           (collect (cons ,(downcase (symbol-name array-symbol)) x)))
+                     ,list-alist)
+          list-alist)))
 
 (defmacro get-path-list (path-symbols)
   "All parameters need to be strings for quri.
@@ -76,25 +78,30 @@
        (,request-type uri :content ,content)
        (parse (,request-type uri :content ,content) :as *output*)))
 
+(eval-when t
+  (defun* (get-symbols-from-list -> proper-list) ((list proper-list))
+    "Extract all symbols from a list"
+    (remove nil `(,@(mapcar (lambda (x) (if (typep x 'symbol)
+                                            x
+                                            nil))
+                            list)))))
+
 (defmacro get-function (function-name)
   "Generates a type-checked REST GET function according get-function class instances."
-  (flet* (((get-symbols-from-list -> proper-list) ((list proper-list))
-           "Extract all symbols from a list"
-           (remove nil `(,@(mapcar (lambda (x) (if (typep x 'symbol) x nil)) list)))))
-         (*let ((path-params proper-list (get-slot function-name 'path))
-                (path-list proper-list `(get-path-list ,path-params))
-                (path-symbols proper-list (get-symbols-from-list path-params))
-                (query-symbols proper-list (get-slot function-name 'query))
-                (query-list proper-list `(get-query ,query-symbols))
-                (typed-lambda-list proper-list
-                                   (typed-lambda-list path-symbols query-symbols))
-                (documentation-string (simple-array character (*))
-                                      (get-slot function-name 'documentation)))
-               `(defun* ,function-name ,typed-lambda-list
-                  ,documentation-string
-                  (let ((uri (get-stacks-uri ,path-list ,query-list)))
-                    (declare (uri-https uri))
-                    (stacks-request get))))))
+  (*let ((path-params proper-list (get-slot function-name 'path))
+         (path-list proper-list `(get-path-list ,path-params))
+         (path-symbols proper-list (get-symbols-from-list path-params))
+         (query-symbols proper-list (get-slot function-name 'query))
+         (query-list proper-list `(get-query ,query-symbols))
+         (typed-lambda-list proper-list
+                            (typed-lambda-list path-symbols query-symbols))
+         (documentation-string (simple-array character (*))
+                               (get-slot function-name 'documentation)))
+        `(defun* ,function-name ,typed-lambda-list
+           ,documentation-string
+           (let ((uri (get-stacks-uri ,path-list ,query-list)))
+             (declare (uri-https uri))
+             (stacks-request get)))))
 
 (defmacro post-function (function-name)
   "Generates a type-checked REST POST function according post-function class instances."
